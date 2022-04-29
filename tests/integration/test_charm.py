d50059f505cb22da021616090d0390f294094b7e
MiaAltieri <mgaltier200@gmail.com>
2022-04-29 09:06:18 +0000

    making integration tests more thorough

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,7 +5,14 @@
 import logging
 
 import pytest
-from helpers import APP_NAME, METADATA, get_address_of_unit, run_mongo_op
+from helpers import (
+    APP_NAME,
+    METADATA,
+    UNIT_IDS,
+    get_address_of_unit,
+    get_leader_id,
+    run_mongo_op,
+)
 from pymongo import MongoClient
 from pytest_operator.plugin import OpsTest
 
@@ -21,7 +28,9 @@ async def test_build_and_deploy(ops_test: OpsTest):
     # build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
     resources = {"mongodb-image": METADATA["resources"]["mongodb-image"]["upstream-source"]}
-    await ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME)
+    await ops_test.model.deploy(
+        charm, resources=resources, application_name=APP_NAME, num_units=len(UNIT_IDS)
+    )
 
     # issuing dummy update_status just to trigger an event
     await ops_test.model.set_config({"update-status-hook-interval": "10s"})
@@ -39,16 +48,36 @@ async def test_build_and_deploy(ops_test: OpsTest):
 
 
 @pytest.mark.abort_on_fail
-async def test_application_is_up(ops_test: OpsTest):
-    address = await get_address_of_unit(ops_test)
+@pytest.mark.parametrize("unit_id", UNIT_IDS)
+async def test_application_is_up(ops_test: OpsTest, unit_id: int):
+    address = await get_address_of_unit(ops_test, unit_id=unit_id)
     response = MongoClient(address, directConnection=True).admin.command("ping")
     assert response["ok"] == 1
 
 
 @pytest.mark.abort_on_fail
-async def test_application_is_up_internally(ops_test: OpsTest):
-    response_obj = await run_mongo_op(ops_test, "rs.status()")
+async def test_application_primary(ops_test: OpsTest):
+    """Tests existience of primary and verifies the application is running as a replica set.
+
+    By retrieving information about the primary this test inherently tests password retrieval.
+    """
+    rs_status = await run_mongo_op(ops_test, "rs.status()")
+    assert rs_status, "mongod had no response for 'rs.status()'"
+
     primary = [
-        member["name"] for member in response_obj["members"] if member["stateStr"] == "PRIMARY"
+        member["name"] for member in rs_status["members"] if member["stateStr"] == "PRIMARY"
     ][0]
-    assert primary == "mongodb-k8s-0.mongodb-k8s-endpoints:27017"
+
+    assert primary, "mongod has no primary on deployment"
+
+    number_of_primaries = 0
+    for member in rs_status["members"]:
+        if member["stateStr"] == "PRIMARY":
+            number_of_primaries += 1
+
+    assert number_of_primaries == 1, "more than one primary in replica set"
+
+    leader_id = await get_leader_id(ops_test)
+    assert (
+        primary == f"mongodb-k8s-{leader_id}.mongodb-k8s-endpoints:27017"
+    ), "primary not leader on deployment"
